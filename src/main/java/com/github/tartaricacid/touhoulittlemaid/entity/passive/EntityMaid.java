37bbfbb28f0b0872a39365edddc2bf24cacc8b41
mikasa <1244456115@qq.com>
2025-12-10 00:39:00 +0800

    Sync with TLM-1.4.6

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/entity/passive/EntityMaid.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/entity/passive/EntityMaid.java
@@ -1101,7 +1101,6 @@ public class EntityMaid extends TamableAnimal implements CrossbowAttackMob, IMai
             this.clearFire();
             this.setTicksFrozen(0);
             this.setSharedFlagOnFire(false);
-            this.getCombatTracker().recheckStatus();
             this.removeAllEffects();
             // 最后父类方法
             super.die(cause);
