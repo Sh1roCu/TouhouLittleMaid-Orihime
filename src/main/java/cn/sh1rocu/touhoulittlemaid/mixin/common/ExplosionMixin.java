27f6fc3ed448555f1e8376700eba8bf5d03ecd61
mikasa <1244456115@qq.com>
2025-12-30 03:56:36 +0800

    Try workflow

--- src/main/java/cn/sh1rocu/touhoulittlemaid/mixin/common/ExplosionMixin.java
+++ src/main/java/cn/sh1rocu/touhoulittlemaid/mixin/common/ExplosionMixin.java
@@ -1,13 +1,10 @@
 package cn.sh1rocu.touhoulittlemaid.mixin.common;
 
 import cn.sh1rocu.touhoulittlemaid.api.event.ExplosionEvents;
-import cn.sh1rocu.touhoulittlemaid.api.extension.IBlock;
-import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
