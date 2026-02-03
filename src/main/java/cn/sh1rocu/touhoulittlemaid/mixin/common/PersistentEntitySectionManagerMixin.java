be4f4257f889e7ad984b057a4302592b07113f43
mikasa <1244456115@qq.com>
2026-02-03 12:01:48 +0800

    Fix mixin

--- src/main/java/cn/sh1rocu/touhoulittlemaid/mixin/common/PersistentEntitySectionManagerMixin.java
+++ src/main/java/cn/sh1rocu/touhoulittlemaid/mixin/common/PersistentEntitySectionManagerMixin.java
@@ -23,7 +23,10 @@ public class PersistentEntitySectionManagerMixin<T extends EntityAccess> {
         }
     }
 
-    @Inject(method = {"method_31854", "method_31863", "method_31864"}, at = @At("TAIL"))
+    // method_31857: processPendingLoads.11
+    // method_31863: addWorldGenChunkEntities.1
+    // method_31864: addLegacyChunkEntities.0
+    @Inject(method = {"method_31857", "method_31863", "method_31864"}, at = @At("TAIL"))
     private static void tlm$addedToWorld(EntityAccess entityAccess, CallbackInfo ci) {
         if (entityAccess instanceof IEntity entity)
             entity.onAddedToWorld();
