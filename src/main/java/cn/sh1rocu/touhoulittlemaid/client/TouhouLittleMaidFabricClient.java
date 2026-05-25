e6b327a0572897431d7ca98609be75cd725877c9
mikasa <1244456115@qq.com>
2026-05-26 00:55:33 +0800

    porting `simplebedrockmodel` module

--- src/main/java/cn/sh1rocu/touhoulittlemaid/client/TouhouLittleMaidFabricClient.java
+++ src/main/java/cn/sh1rocu/touhoulittlemaid/client/TouhouLittleMaidFabricClient.java
@@ -4,8 +4,6 @@ import cn.sh1rocu.touhoulittlemaid.api.event.KeyInputCallback;
 import cn.sh1rocu.touhoulittlemaid.api.event.PlaySoundEvent;
 import cn.sh1rocu.touhoulittlemaid.api.event.PlaySoundSourceEvent;
 import cn.sh1rocu.touhoulittlemaid.api.event.RenderHandEvent;
-import cn.sh1rocu.touhoulittlemaid.api.extension.IItemRenderer;
-import com.github.tartaricacid.simplebedrockmodel.client.manager.BedrockEntityModelRegisterEvent;
 import com.github.tartaricacid.touhoulittlemaid.TouhouLittleMaid;
 import com.github.tartaricacid.touhoulittlemaid.TouhouLittleMaidClient;
 import com.github.tartaricacid.touhoulittlemaid.api.event.InteractMaidEvent;
@@ -25,15 +23,14 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientEntityEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
-import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
 import net.fabricmc.fabric.api.client.rendering.v1.ClientTooltipComponentCallback;
 import net.fabricmc.fabric.api.client.rendering.v1.level.LevelRenderEvents;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.fabric.api.event.Event;
-import net.minecraft.core.registries.BuiltInRegistries;
 
-import static cn.sh1rocu.touhoulittlemaid.TouhouLittleMaidFabric.*;
+import static cn.sh1rocu.touhoulittlemaid.TouhouLittleMaidFabric.LOW;
+import static cn.sh1rocu.touhoulittlemaid.TouhouLittleMaidFabric.LOWEST;
 
 public class TouhouLittleMaidFabricClient implements ClientModInitializer {
     @Override
@@ -43,16 +40,7 @@ public class TouhouLittleMaidFabricClient implements ClientModInitializer {
         ClientExtensionsEvent.RegisterClientExtensions();
         InfoGetManager.onClientSetup();
 
-        com.github.tartaricacid.simplebedrockmodel.client.ClientSetupEvent.onClientSetup();
-
         ClientReloadListenerRegistry.onRegisterClientReloadListeners();
-
-        BuiltInRegistries.ITEM.stream().filter(item -> item instanceof IItemRenderer).forEach(clientEx ->
-                BuiltinItemRendererRegistry.INSTANCE.register(clientEx,
-                        (stack, mode, matrices, vertexConsumers, light, overlay) ->
-                                ((IItemRenderer) clientEx).getCustomRenderer().renderByItem(stack, mode, matrices, vertexConsumers, light, overlay)));
-
-        BedrockEntityModelRegisterEvent.CALLBACK.register(BedrockModelLoader::onRegisterBedrockModelRenderers);
 
         ItemTooltipCallback.EVENT.addPhaseOrdering(Event.DEFAULT_PHASE, LOW);
         ItemTooltipCallback.EVENT.addPhaseOrdering(LOW, LOWEST);
@@ -73,11 +61,6 @@ public class TouhouLittleMaidFabricClient implements ClientModInitializer {
         LevelRenderEvents.AFTER_SOLID_FEATURES.register(ScrollRenderEvent::onRenderWorldLastEvent);
         ScreenEvents.AFTER_INIT.register(ShowOptifineScreen::showOptifineWarning);
 
-        RenderMaidEvent.CALLBACK.register(HIGHEST, SpecialMaidRenderEvent::onRenderPlayerNamedMaid);
-        RenderMaidEvent.CALLBACK.register(Event.DEFAULT_PHASE, SpecialMaidRenderEvent::onRenderEncryptNamedMaid);
-        RenderMaidEvent.CALLBACK.register(LOW, SpecialMaidRenderEvent::onRenderNormalNamedMaid);
-        RenderMaidEvent.CALLBACK.register(LOWEST, SpecialMaidRenderEvent::onRenderEasterEggModel);
-
         LevelRenderEvents.AFTER_SOLID_FEATURES.register(WirelessIORenderEvent::onRender);
         ClientSetupEvent.onClientSetup();
         ClientSetupEvent.onRegisterGuiLayers();
@@ -85,7 +68,6 @@ public class TouhouLittleMaidFabricClient implements ClientModInitializer {
         InitContainerGui.clientSetup();
         InitEntitiesRender.onEntityRenderers();
         InitEntitiesRender.onRegisterLayers();
-        ModelLoadingPlugin.register(new InitSpecialItemRender());
         ClientEntityEvents.ENTITY_LOAD.register(EntityCacheUtil::onChangeDim);
         LevelRenderEvents.AFTER_SOLID_FEATURES.register(DebugClientRenderEvent::onRender);
         ClientTickEvents.START_CLIENT_TICK.register(ClientTickEvent::onClientTick);
