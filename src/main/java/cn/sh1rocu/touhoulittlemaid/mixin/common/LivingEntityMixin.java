219aed65dd3747ab9b9b3663144972c979a7868a
mikasa <1244456115@qq.com>
2026-05-26 10:43:50 +0800

    refactor: use EntitySleepEvents from Fabric to impl custom-bed checking instead of mixin

--- src/main/java/cn/sh1rocu/touhoulittlemaid/mixin/common/LivingEntityMixin.java
+++ src/main/java/cn/sh1rocu/touhoulittlemaid/mixin/common/LivingEntityMixin.java
@@ -3,16 +3,12 @@ package cn.sh1rocu.touhoulittlemaid.mixin.common;
 import cn.sh1rocu.touhoulittlemaid.api.event.LivingAttackEvent;
 import cn.sh1rocu.touhoulittlemaid.api.event.LivingDamageEvent;
 import cn.sh1rocu.touhoulittlemaid.api.event.LivingHurtEvent;
-import cn.sh1rocu.touhoulittlemaid.api.extension.IBedBlock;
 import cn.sh1rocu.touhoulittlemaid.util.forge.EventHooks;
 import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
@@ -20,10 +16,6 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BedBlock;
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -33,13 +25,8 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Optional;
-
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
-    @Shadow
-    public abstract Optional<BlockPos> getSleepingPos();
-
     @Shadow
     public abstract ItemStack getUseItem();
 
@@ -80,78 +67,6 @@ public abstract class LivingEntityMixin extends Entity {
         if (event.isCanceled())
             return 0;
         return event.getAmount();
-    }
-
-    @Inject(
-            method = "startSleeping",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/entity/LivingEntity;setPose(Lnet/minecraft/world/entity/Pose;)V"
-            )
-    )
-    private void tlm$startSleeping(BlockPos pos, CallbackInfo ci) {
-        BlockState state = this.level().getBlockState(pos);
-        if (!(state.getBlock() instanceof BedBlock) && state.getBlock() instanceof IBedBlock bedBlock) {
-            if (bedBlock.tlm$isBed(state, this.level(), pos, (LivingEntity) (Object) this))
-                this.level().setBlock(pos, state.setValue(BedBlock.OCCUPIED, true), 3);
-        }
-    }
-
-    @Inject(
-            method = "checkBedExists",
-            at = @At("HEAD"),
-            cancellable = true
-    )
-    private void tlm$checkBedExists(CallbackInfoReturnable<Boolean> cir) {
-        Optional<BlockPos> blockPos = this.getSleepingPos();
-        if (blockPos.isPresent()) {
-            BlockState state = this.level().getBlockState(blockPos.get());
-            if (state.getBlock() instanceof IBedBlock bedBlock)
-                cir.setReturnValue(bedBlock.tlm$isBed(state, this.level(), blockPos.get(), (LivingEntity) (Object) this));
-            else cir.setReturnValue(state.getBlock() instanceof BedBlock);
-        }
-    }
-
-    @Inject(
-            method = "stopSleeping",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/entity/LivingEntity;position()Lnet/minecraft/world/phys/Vec3;"
-            )
-    )
-    private void tlm$stopSleeping(CallbackInfo ci) {
-        Optional<BlockPos> sleepingPos = this.getSleepingPos();
-        sleepingPos.filter(this.level()::hasChunkAt).ifPresent((blockPos) -> {
-            BlockState blockState = this.level().getBlockState(blockPos);
-            if (!(blockState.getBlock() instanceof BedBlock) && blockState.getBlock() instanceof IBedBlock bedBlock) {
-                if (bedBlock.tlm$isBed(blockState, this.level(), blockPos, (LivingEntity) (Object) this)) {
-                    Direction direction = blockState.getValue(BedBlock.FACING);
-                    this.level().setBlock(blockPos, blockState.setValue(BedBlock.OCCUPIED, false), 3);
-                    Vec3 vec3 = BedBlock.findStandUpPosition(this.getType(), this.level(), blockPos, direction, this.getYRot()).orElseGet(() -> {
-                        BlockPos blockPos2 = blockPos.above();
-                        return new Vec3((double) blockPos2.getX() + (double) 0.5F, (double) blockPos2.getY() + 0.1, (double) blockPos2.getZ() + (double) 0.5F);
-                    });
-                    Vec3 vec32 = Vec3.atBottomCenterOf(blockPos).subtract(vec3).normalize();
-                    float f = (float) Mth.wrapDegrees(Mth.atan2(vec32.z, vec32.x) * (double) (180F / (float) Math.PI) - (double) 90.0F);
-                    this.setPos(vec3.x, vec3.y, vec3.z);
-                    this.setYRot(f);
-                    this.setXRot(0.0F);
-                }
-            }
-        });
-    }
-
-    @Inject(
-            method = "getBedOrientation",
-            at = @At("HEAD"),
-            cancellable = true
-    )
-    private void tlm$getBedOrientation(CallbackInfoReturnable<Direction> cir) {
-        BlockPos blockPos = this.getSleepingPos().orElse(null);
-        if (blockPos == null) return;
-        BlockState state = this.level().getBlockState(blockPos);
-        if (state.getBlock() instanceof IBedBlock bedBlock)
-            cir.setReturnValue(!bedBlock.tlm$isBed(state, this.level(), blockPos, (LivingEntity) (Object) this) ? Direction.UP : state.getValue(HorizontalDirectionalBlock.FACING));
     }
 
     @WrapOperation(method = "completeUsingItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;finishUsingItem(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/LivingEntity;)Lnet/minecraft/world/item/ItemStack;"))
