b6449009cd9e0e970a4cb33226cd08cb608cbe72
mikasa <1244456115@qq.com>
2026-05-26 20:09:16 +0800

    refactor: use mixin to set the GeckoEntity attach associated with maid

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/TouhouLittleMaidClient.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/TouhouLittleMaidClient.java
@@ -1,12 +1,7 @@
 package com.github.tartaricacid.touhoulittlemaid;
 
-import cn.sh1rocu.touhoulittlemaid.api.event.EntityJoinLevelEvent;
-import com.github.tartaricacid.touhoulittlemaid.api.entity.IMaid;
-import com.github.tartaricacid.touhoulittlemaid.client.entity.GeckoMaidEntity;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.Mob;
 
 @Environment(EnvType.CLIENT)
 public class TouhouLittleMaidClient {
@@ -16,16 +11,18 @@ public class TouhouLittleMaidClient {
 
     private static void registerClientOnly() {
         // 这个仅用于客户端，所以不需要在服务端注册
-        EntityJoinLevelEvent.CALLBACK.register(event -> {
-            Entity clientEntity = event.getEntity();
-            if (!clientEntity.level.isClientSide())
-                return;
-            if (clientEntity instanceof Mob mob) {
-                IMaid maid = IMaid.convert(mob);
-                if (maid != null) {
-                    clientEntity.setAttached(GeckoMaidEntity.TYPE, new GeckoMaidEntity(mob, maid));
-                }
-            }
-        });
+
+        // 弃用，改为使用mixin在实体生成时赋予
+//        EntityJoinLevelEvent.CALLBACK.register(event -> {
+//            Entity clientEntity = event.getEntity();
+//            if (!clientEntity.level.isClientSide())
+//                return;
+//            if (clientEntity instanceof Mob mob) {
+//                IMaid maid = IMaid.convert(mob);
+//                if (maid != null) {
+//                    clientEntity.setAttached(GeckoMaidEntity.TYPE, new GeckoMaidEntity(mob, maid));
+//                }
+//            }
+//        });
     }
 }
