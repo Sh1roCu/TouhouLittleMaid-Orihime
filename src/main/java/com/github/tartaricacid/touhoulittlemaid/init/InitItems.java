cdc3d5954337105debaec1929936fb266d95ff41
mikasa <1244456115@qq.com>
2026-07-02 22:06:39 +0800

    fix(#34): EntityTombstone generation - the position where the EntityTombstone generated is not correct - EntityTombstone don't be generated after the maid dying when the level-type of the world is not Superflat

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/init/InitItems.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/init/InitItems.java
@@ -9,7 +9,6 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
